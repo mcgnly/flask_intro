ca69db6445bfc9eff57d2141f177d532cb200153
mcgnly <katie@mcgnly.com>
2016-01-18 17:04:26 +0100

    update index view to show posts

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def home():
     cur = g.db.execute('select * from posts')
     posts = [dict(title = row[0], description=row[1]) for row in cur.fetchall()]
     g.db.close()
-	return render_template ("index.html", posts = posts)
+    return render_template ("index.html", posts = posts)
 
 #route for welcome
 @app.route('/welcome')
