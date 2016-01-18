ee0ca679de1f255561ba0f5520ef5f2677432112
mcgnly <katie@mcgnly.com>
2016-01-18 12:16:20 +0100

    replace bootstrap with correct file add login page route add login page html

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+#route for home
 @app.route('/')
 def home():
 	return "hello world"
 
+#route for welcome
 @app.route('/welcome')
 def welcomw():
 	return render_template ("welcome.html")
 
+# route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
+
+
+#start the server with 'run()' method
 if __name__ == '__main__':
 	app.run(debug=True)
