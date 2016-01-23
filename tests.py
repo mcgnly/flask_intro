f0a01c594662a19a91c7f26bca9c517568742ef7
mcgnly <katie@mcgnly.com>
2016-01-23 19:05:37 +0100

    add tests to check logout works and logging in w bad credentials works like it's supposed to

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ class FlaskTestCase(unittest.TestCase):
     def test_login_page_loads(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
-        self.assertTrue(('Please login'.encode('utf-8')) in response.data)
+        self.assertTrue(('Please login'.encode('utf-8')) in response.data) #have to add the .encode('utf-8') step because of weirdness in py2 vs py3
 
     #ensure login behaves correctly w correct credentials
     def test_correct_login(self):
@@ -26,7 +26,26 @@ class FlaskTestCase(unittest.TestCase):
         self.assertIn(('you were just logged in'.encode('utf-8')), response.data)
 
     #ensure login works right w incorrect credentials
+    def test_incorrect_login(self):
+        tester = app.test_client(self)
+        response = tester.post(
+        	'/login', 
+        	data =dict(username = "notadmin", password ="notadmin"),
+        	follow_redirects =True)
+        self.assertIn(('Invalid Credentials. Please try again.'.encode('utf-8')), response.data)
 
     #ensure logout works right
+    def test_logout_page_loads(self):
+        tester = app.test_client(self)
+        #first we have to log in, to test logout
+        response = tester.post(
+        	'/login', 
+        	data =dict(username = "admin", password ="admin"),
+        	follow_redirects =True)
+        #now that we're in, we can test geting out
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertTrue(('you logged out'.encode('utf-8')) in response.data) #have to add the .encode('utf-8') step because of weirdness in py2 vs py3
+
+
 if __name__ == '__main__':
     unittest.main()
